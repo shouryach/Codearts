093c84ad499711946145a323cf4c8da86638da61
shouryac <magentoshourya@gmail.com>
2023-05-05 22:02:48 +0530

    Header File Cleanup

--- wp-content/themes/codearts/header.php
+++ wp-content/themes/codearts/header.php
@@ -116,106 +116,11 @@
 <?php 
   if( 'post' == get_post_type() || 'all_portfolios' == get_post_type() )
     { if(is_single()){ ?>
-   <section class="custom-inner-banner">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="custom-inner-banner-content-wrap">
-            <nav
-              style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
-              aria-label="breadcrumb">
-              <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="<?php echo site_url(); ?>">HOME</a></li>
-              <li class="breadcrumb-item">
-              	<a href="<?php
-                              if('post' == get_post_type())
-                                {
-                                echo site_url('blogs');
-                                }
-                                else if('all_portfolios' == get_post_type())
-                                {
-                                echo site_url('portfolios');
-                                }
-                                ?>">
-                        <?php
-                        if('post' == get_post_type())
-                        {
-                          echo "Blogs";
-                        }
-                        else if('all_portfolios' == get_post_type())
-                        {
-                          echo "Portfolios";
-                        }
-                  ?>
-                  </a></li>
-                  <li class="breadcrumb-item active" aria-current="page"><?php echo get_the_title(); ?></li>
-              </ol>
-            </nav>
-            <?php $short_title = get_field('all_pages_sub_title'); if( $short_title != ''){ ?>
-            <h2><?php echo $short_title; ?></h2>
-            <?php } ?>
-            <div class="inner-banner-btn">
-              <a class="custom-button-alt" href="<?php echo esc_url(home_url('services')); ?>">
-                Our Services
-                <span><i class="las la-long-arrow-alt-right"></i></span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+   
 <?php } else if(is_archive() || is_home() ){  ?>
-	<section class="custom-inner-banner">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="custom-inner-banner-content-wrap">
-            <nav
-              style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
-              aria-label="breadcrumb">
-              <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="<?php echo site_url(); ?>">HOME</a></li>
-              <li class="breadcrumb-item">
-              	<a href="<?php
-                              if('post' == get_post_type())
-                                {
-                                echo site_url('blogs');
-                                }
-                                else if('all_portfolios' == get_post_type())
-                                {
-                                echo site_url('portfolios');
-                                }
-                                ?>">
-                        <?php
-                        if('post' == get_post_type())
-                        {
-                          echo "Blogs";
-                        }
-                        else if('all_portfolios' == get_post_type())
-                        {
-                          echo "Portfolios";
-                        }
-                  ?> </a>
-                  </li>
-                  <?php  if(is_tax()){ printf( __( '%s', 'twentyten' ), '<li class="breadcrumb-item active" aria-current="page">Category: ' . single_cat_title( '', false ) . '</li>' ); } ?>
-              </ol>
-            </nav>
-            <?php $short_title = get_field('all_pages_sub_title'); if( $short_title != ''){ ?>
-            <h2><?php echo $short_title; ?></h2>
-            <?php } ?>
-            <div class="inner-banner-btn">
-              <a class="custom-button-alt" href="<?php echo esc_url(home_url('services')); ?>">
-                Our Services
-                <span><i class="las la-long-arrow-alt-right"></i></span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+	
 <?php } } else if(!is_front_page() && !is_404() ) { ?>
+  <!--Unable to delete this legendary code-->
   <section class="custom-inner-banner">
     <div class="container">
       <div class="row">
